c01f7db1da501dc981ad9a9d79a78490589e5c29
Caden Kim <ckim6@zagmail.gonzaga.edu>
2023-04-28 21:27:04 -0700

    Still Working

--- elgamal.py
+++ elgamal.py
@@ -19,6 +19,22 @@ def param_gen(size):
     ZZn = IntegerModRing(p)
 
     totient = p - 1
+    a = 3
+    e = totient/2
+    while(pow(a, e, p) != totient):
+       a = a + 2
+    
+    a = ZZn(a)
+    return p, a
+
+def key_gen(p, a):
+    ZZn = IntegerModRing(p)
+    A = ZZn.random_element()
+    B = a^A
+
+    return A, B
+
+def sign(p, a, A):
     
 
 #transforms msg_in to a base 256 integer which it then returns
